6196813ec46aaf85b53e07a06cdf0e0236c48a41
Juan Miguel Torres <jm@Juans-MacBook-Pro.local>
2019-11-18 10:03:42 +0800

    first commit

---
+++ Fligno/FlignoServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Fligno;
+
+use Illuminate\Support\ServiceProvider;
+
+class FlignoServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $folders = array_diff(scandir(__DIR__), array('..', '.', 'FlignoServiceProvider.php'));
+        $excludeFolder = [];
+
+        foreach ($folders as $folder) {
+            if (strpos($folder, 'Trait') || in_array($folder, $excludeFolder))
+                continue;
+
+            $serviceProvider = "Fligno\\{$folder}\\{$folder}ServiceProvider";
+            $this->app->register($serviceProvider);
+        }
+    }
+}
